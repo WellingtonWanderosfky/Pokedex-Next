4d2da0e97ae0408721304a47b8f1136dc068dc51
Wellington Pedro <wellington.pedro@agenciam3.com>
2023-02-13 22:33:57 -0300

    fix: ajuste efeito de blur

--- pokedexnext/src/components/TypesPokemons/TypesPokemons.tsx
+++ pokedexnext/src/components/TypesPokemons/TypesPokemons.tsx
@@ -1,31 +1,75 @@
 
-
+import styles from "./TypesPokemons.module.css";
 interface TypesPokemonsProps {
     typePokemon: string;
 }
 
 const TypesPokemons = ({typePokemon} : TypesPokemonsProps) => {
     console.log("typePokemon:",typePokemon);
-  return (
-    <>
-        {(() => {
-        switch (typePokemon) {
-          case 'grass':
-            return <img src="https://via.placeholder.com/50x30/00FF00/000000?text=Grass" alt="Grass flag" />;
-          case 'fire':
-            return <img src="https://via.placeholder.com/50x30/FF0000/000000?text=Fire" alt="Fire flag" />;
-          case 'water':
-            return <img src="https://via.placeholder.com/50x30/0000FF/000000?text=Water" alt="Water flag" />;
-          case 'electric':
-            return <img src="https://via.placeholder.com/50x30/FFFF00/000000?text=Electric" alt="Electric flag" />;
-          case 'normal':
-            return <img src="https://via.placeholder.com/50x30/A8A878/000000?text=Normal" alt="Normal flag" />;
-          default:
-            return <img src="https://via.placeholder.com/50x30/808080/000000?text=Other" alt="Other flag" />;
-        }
-      })()}
-    </>
-  )
+    const types = [
+        {
+          type: "grass",
+          title: "Grass",
+          imageUrl: "/PokemonsTypes/grass.svg",
+          color: "#1CD80E"
+        }, {
+          type: "fire",
+          title: "Fire",
+          imageUrl: "/PokemonsTypes/fire.svg",
+          color: "#FF9900"
+        }, {
+          type: "water",
+          title: "Water",
+          imageUrl: "/PokemonsTypes/water.svg",
+          color: "#14A8FF"
+        }, {
+          type: "electric",
+          title: "Electric",
+          imageUrl: "/PokemonsTypes/electric.svg",
+          color: "#FFDE00"
+        },
+        {
+            type: "bug",
+            title: "Bug",
+            imageUrl: "/PokemonsTypes/bug.svg",
+            color: "#7BCF00"
+        },
+        {
+          type: "normal",
+          title: "Normal",
+          imageUrl: "/PokemonsTypes/normal.svg",
+          color: "#7BCF00"
+        },
+        {
+          type: "poison",
+          title: "Poison",
+          imageUrl: "/PokemonsTypes/poison.svg",
+          color: "#F149FF"
+        },
+        {
+          type: "dragon",
+          title: "Dragon",
+          imageUrl: "/PokemonsTypes/dragon.svg",
+          color: "#0076FF"
+        },
+        {
+          type: "dark",
+          title: "Dark",
+          imageUrl: "/PokemonsTypes/dark.svg",
+          color: "#5A566A"
+        },
+      ]
+      
+      const currentType = types.find(({ type }) => type === typePokemon)
+
+      if(!currentType) return <></>
+      
+      const {title,imageUrl , color } = currentType;
+      
+      return <button style={{backgroundColor : color}} className={styles.buttonContainer}>
+        <img src={imageUrl}></img>
+        {title}
+        </button>
 }
 
 export default TypesPokemons
