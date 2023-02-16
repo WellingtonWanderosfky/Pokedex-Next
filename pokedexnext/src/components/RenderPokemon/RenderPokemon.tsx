defd94f9012a4e300651a6d38ebe806456e59b41
Wellington Pedro <wellington.pedro@agenciam3.com>
2023-02-16 09:14:02 -0300

    fix: ver mais

--- pokedexnext/src/components/RenderPokemon/RenderPokemon.tsx
+++ pokedexnext/src/components/RenderPokemon/RenderPokemon.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import CardPokemon from "../CardPokemon/CardPokemon";
 import styles from "./RenderPokemon.module.css";
 import requestPokemon from "./utils/requestPokemon";
-
 interface Pokemon{
     name: string;
     url: string;
@@ -11,12 +10,19 @@ interface Pokemon{
 const  RenderPokemon =  () => {
 
     const [result, setResult] = useState<Pokemon[]>([]);
-    const maxPokemon = 50;
+    const [offset, setOffset] = useState(0);
+    const maxPokemon = 9;
+
+    const handleLoadMore = async () => {
+        setOffset(offset + 9);
+        const resultRequest = await requestPokemon(maxPokemon, offset + 9);
+        setResult([...result, ...(resultRequest?.results ?? [])]);
+    }
 
     useEffect(() => {
         const pokemonData = async () =>{
-            const result = await requestPokemon(maxPokemon);
-            setResult(result?.results ?? []);
+            const resultRequest = await requestPokemon(maxPokemon, offset);
+            setResult(resultRequest?.results ?? [])
         } 
         pokemonData();
     }, []);
@@ -24,13 +30,18 @@ const  RenderPokemon =  () => {
     //
 
     return (
-        <div className={styles.containerRenderPokemons}>
-            {result.map((pokemon, index) => {
-                return (
-                    <CardPokemon url={pokemon.url} name={pokemon.name} key={index}></CardPokemon> 
-                )
-            })}
-        </div>
+        <>
+            <div className={styles.containerRenderPokemons}>
+                {result.map((pokemon, index) => {
+                    return (
+                        <CardPokemon url={pokemon.url} name={pokemon.name} key={index}></CardPokemon> 
+                    )
+                })}
+            </div>
+            <div className={styles.buttonContainerLoading}>
+                <button className={styles.buttonLoading} onClick={handleLoadMore}>Carregar mais...</button>
+            </div>
+        </>
   )
 }
 
